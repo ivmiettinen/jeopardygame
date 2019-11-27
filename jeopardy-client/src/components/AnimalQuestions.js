7e20faa87fbbdcfd937e00c4d212606277d70b81
Ilkka Miettinen <ivmiettinen@gmail.com>
2019-11-27 19:06:31 +0200

    fixedComponentInitials

--- jeopardy-client/src/components/AnimalQuestions.js
+++ jeopardy-client/src/components/AnimalQuestions.js
@@ -3,31 +3,24 @@ import AnimalQuestionItem from './AnimalQuestionItem';
 import './AnimalQuestions.css';
 
 class AnimalQuestions extends Component {
+  state = {
+    animalQuestions: []
+  };
 
-    state = {
-        animalQuestions: []
+  componentDidMount() {
+    fetch('api/questions/elaimet')
+      .then(res => res.json())
+      .then(res => this.setState({ animalQuestions: res }))
+      .catch(() => this.setState({ hasErrors: true }));
+  }
 
-    }
+  render() {
+    const animalItem = this.state.animalQuestions.map(question => {
+      return <AnimalQuestionItem data={question} key={question.id} />;
+    });
 
-    componentDidMount(){
-        fetch("api/questions/elaimet")
-        .then(res => res.json())
-        .then(res => this.setState({animalQuestions: res}))
-        .catch(()=> this.setState({hasErrors: true}));
-    }
-
-    render() {
-        const animalItem = this.state.animalQuestions
-        .map((question) =>{
-            return <AnimalQuestionItem data = {question} key = {question.id}/>
-        } )
-
-        return (
-            <div className="MainDiv">
-                {animalItem}
-            </div>
-        );
-    }
+    return <div className='MainDiv'>{animalItem}</div>;
+  }
 }
 
 export default AnimalQuestions;
